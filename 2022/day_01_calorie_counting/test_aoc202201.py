b4e635d2297845ae9f2087efe02a359d492e8ae2
Norbert Varga <vnorbix@gmail.com>
2022-12-05 11:02:44 +0100

    Fixed paths for day 1 & 2.

--- 2022/day_01_calorie_counting/test_aoc202201.py
+++ 2022/day_01_calorie_counting/test_aoc202201.py
@@ -4,14 +4,15 @@ import pytest
 
 from aoc202201 import parse_input, part1, part2
 
+PUZZLE_DIR = pathlib.Path(__file__).parent
 
 @pytest.fixture
 def example1():
-    return parse_input(pathlib.Path('example1.txt').read_text().strip())
+    return parse_input(pathlib.Path(PUZZLE_DIR / 'example1.txt').read_text().strip())
 
 
 def test_parse_input():
-    puzzle_input = parse_input(pathlib.Path('example1.txt').read_text().strip())
+    puzzle_input = parse_input(pathlib.Path(PUZZLE_DIR / 'example1.txt').read_text().strip())
     assert puzzle_input == [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
 
 
--- 2022/day_02_rock_paper_scissors/test_aoc202202.py
+++ 2022/day_02_rock_paper_scissors/test_aoc202202.py
@@ -4,28 +4,29 @@ import pytest
 
 from aoc202202 import parse_input_part1, part1, part2, Round, GameAction, parse_input_part2
 
+PUZZLE_DIR = pathlib.Path(__file__).parent
 
 @pytest.fixture
 def example_part1():
-    return parse_input_part1(pathlib.Path('example1.txt').read_text().strip())
+    return parse_input_part1(pathlib.Path(PUZZLE_DIR / 'example1.txt').read_text().strip())
 
 
 @pytest.fixture
 def example_part2():
-    return parse_input_part1(pathlib.Path('example1.txt').read_text().strip())
+    return parse_input_part2(pathlib.Path(PUZZLE_DIR / 'example1.txt').read_text().strip())
 
 
 def test_parse_input_part1():
-    puzzle_input = parse_input_part1(pathlib.Path('example1.txt').read_text().strip())
+    puzzle_input = parse_input_part1(pathlib.Path(PUZZLE_DIR / 'example1.txt').read_text().strip())
     assert puzzle_input == [
-        Round(GameAction.ROCK, GameAction.SCISSORS),
-        Round(GameAction.SCISSORS, GameAction.ROCK),
-        Round(GameAction.PAPER, GameAction.PAPER)
+        Round(GameAction.ROCK, GameAction.PAPER),
+        Round(GameAction.PAPER, GameAction.ROCK),
+        Round(GameAction.SCISSORS, GameAction.SCISSORS)
     ]
 
 
 def test_parse_input_part2():
-    puzzle_input = parse_input_part2(pathlib.Path('example1.txt').read_text().strip())
+    puzzle_input = parse_input_part2(pathlib.Path(PUZZLE_DIR / 'example1.txt').read_text().strip())
     assert puzzle_input == [
         Round(GameAction.ROCK, GameAction.ROCK),
         Round(GameAction.PAPER, GameAction.ROCK),
